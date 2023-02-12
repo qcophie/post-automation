67ae2705caef954be116d8195b5001b4c2e91ca9
josh <joshqcophie@gmail.com>
2023-02-12 22:47:20 +0000

    fixed bugs, completed test case, and tested using postman

--- posts/utils.py
+++ posts/utils.py
@@ -1,4 +1,5 @@
-import os, tweepy 
+import os, tweepy
+
 
 # Generic success response
 def success_response(message="Data returned successfully", data=[]):
@@ -10,24 +11,27 @@ def error_response(message="An error occurred", response_code='101'):
 	return {"response_code": response_code, "message": message}
 
 
-def validate_post_content(request):
-	# Get content from User
-	content = request.POST.get("content")
+def validate_post_content(content):
 	
 	# Check if content is available
 	if not content:
 		return {"error": True, "error_message": "Please provide content to post", "error_code": 101}
 	
+	return {"error": False}
+
 
 def tweepy_fn(content):
 	CUSTOMER_API_KEY = os.environ.get("CUSTOMER_API_KEY")
 	CUSTOMER_API_KEY_SECRET = os.environ.get("CUSTOMER_API_KEY_SECRET")
 	CUSTOMER_ACCESS_TOKEN = os.environ.get("CUSTOMER_ACCESS_TOKEN")
 	CUSTOMER_ACCESS_TOKEN_SECRET = os.environ.get("CUSTOMER_ACCESS_TOKEN_SECRET")
-
+	
 	auth = tweepy.OAuthHandler(CUSTOMER_API_KEY, CUSTOMER_API_KEY_SECRET, CUSTOMER_ACCESS_TOKEN, CUSTOMER_ACCESS_TOKEN_SECRET)
 	twitter_api = tweepy.API(auth)
-
-	tweet = twitter_api.update_status(content)
-	print("tweet", tweet)
-	return ''
+	
+	try:
+		twitter_api.update_status(content)
+		return "Successful"
+	
+	except Exception as e:
+		return f"Failed with Error: {e}"
--- posts/views.py
+++ posts/views.py
@@ -9,19 +9,20 @@ from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_400_BAD_RE
 class PostContent(APIView):
     
     def post(self, request, *args, **kwargs):
-        content = request.data.get('content', '')
-        my_response = validate_post_content(request)
+        content = request.data.get('content')
+        my_response = validate_post_content(content)
         
         if my_response.get("error"):
             return Response(error_response(my_response.get("error_message"), my_response.get("error_code")), status=HTTP_400_BAD_REQUEST)
         
         # Call Fortune's Function
-        tweet = tweepy_fn(content="")
+        response = tweepy_fn(content)
         
-        # For success response
-        return Response(success_response("Post Created Successfully"), status=HTTP_200_OK)
-    
+        if response == "Successful":
+            # For success response
+            return Response(success_response("Post Created Successfully"), status=HTTP_200_OK)
+        
         # For error response
-        return Response(error_response("Post not Created"), status=HTTP_400_BAD_REQUEST)
+        return Response(error_response("Post could not be created at this time."), status=HTTP_400_BAD_REQUEST)
         
     
